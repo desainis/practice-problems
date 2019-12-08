e1bb5da09aa348b36fc1cf10478b95c150e672e9
Nishant Desai <nishant.desai@mail.utoronto.ca>
2019-12-08 14:02:25 -0500

    added inital problems

---
+++ longest-substring-without-repeating-characters/template.py
@@ -0,0 +1,12 @@
+'''
+Given a string, find the length of the longest substring without repeating characters.
+
+Can you find a solution in linear time?
+'''
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+    # Fill this in.
+
+print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+# 10
